e4cd9e0d53be6e1c7af8231895a76ea400bc9b4f
ssa <sahayaanishjb@gmail.com>
2025-11-23 21:01:26 +0530

    update base URL for API to production environment

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,8 +2,8 @@
 
 const String appName = 'APS';
 const String appVersion = '1.0.0';
-const String baseUrl = 'https://staging-admin.apshome.in/api';
-// const String baseUrl = 'https://dashboard.apshome.in/api';
+// const String baseUrl = 'https://staging-admin.apshome.in/api';
+const String baseUrl = 'https://dashboard.apshome.in/api';
 const String primaryColor = '#d50009';
 const String secondaryColor = '#d50009';
 const String accentColor = '#d50009';
